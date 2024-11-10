261b56b25e3a5b61470dbe68cb0414befd3210e3
Lee <lmbrooksit@icloud.com>
2024-11-10 13:28:37 -0500

    openpyxl

---
+++ hello.py
@@ -0,0 +1,16 @@
+print('hi')
+from openpyxl.workbook import Workbook # type: ignore
+from openpyxl import load_workbook # type: ignore
+
+#Create a workbook object
+# wb = Workbook()
+
+
+
+# load existing spreadsheet
+
+wb = load_workbook('hello.xlsx')
+
+ws = wb.active
+
+print("hello World!")
